d7c757b28bce911c572e13ffe108211073d2cc9e
Gideon Kemboi <gedionkemboi18@gmail.com>
2024-07-05 11:34:44 +0300

    Add notification routes and dummy controllers

---
+++ controllers/notifications.js
@@ -0,0 +1,7 @@
+exports.getNotifications = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Get Notifications");
+};
+
+exports.updateNotification = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Update Notification");
+};
---
+++ routes/notifications.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router;
+
+const notificationControllers = require("../controllers/notifications.js");
+
+router.get("/notifications", notificationControllers.getNotifications);
+
+router.put(
+  "/notifications/:notificationId/read",
+  notificationControllers.updateNotification
+);
