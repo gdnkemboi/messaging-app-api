3fe9bdcad291d82ade24459158b0433753e18d03
Gideon Kemboi <gedionkemboi18@gmail.com>
2024-07-05 11:26:04 +0300

    Add group routes and dummy controllers

---
+++ controllers/groups.js
@@ -0,0 +1,27 @@
+exports.createGroup = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Create Group");
+};
+
+exports.getUserGroups = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Get User Group");
+};
+
+exports.getGroupDetails = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Get Group Details");
+};
+
+exports.updateGroupDetails = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Update Group Details");
+};
+
+exports.deleteGroup = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Delete Group");
+};
+
+exports.addMember = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Add Member to Group");
+};
+
+exports.removeMember = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Remove Member from Group");
+};
---
+++ routes/groups.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router;
+
+const groupControllers = require("../controllers/groups");
+
+router.post("/groups", groupControllers.createGroup);
+
+router.get("/groups", groupControllers.getUserGroups);
+
+router.get("/groups/:groupId", groupControllers.getGroupDetails);
+
+router.put("/groups/:groupId", groupControllers.updateGroupDetails);
+
+router.delete("/groups/:groupId", groupControllers.deleteGroup);
+
+router.post("/groups/:groupId/members", groupControllers.addMember);
+
+router.delete(
+  "/groups/:groupId/members/:userId",
+  groupControllers.removeMember
+);
+
+module.exports = router;
