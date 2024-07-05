5bc8fddfd1aca55ea09717e30c621c2b97cb6c88
Gideon Kemboi <gedionkemboi18@gmail.com>
2024-07-05 11:02:39 +0300

    Add conversation routes and dummy controllers

---
+++ controllers/conversations.js
@@ -0,0 +1,15 @@
+exports.createConversation = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Create Conversation");
+};
+
+exports.getConversations = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Get Conversations");
+};
+
+exports.getConversationDetail = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Get Conversation Detail");
+};
+
+exports.deleteConversation = (req, res, next) => {
+  res.send("NOT IMPLEMENTED: Delete Conversation");
+};
---
+++ routes/conversations.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router;
+
+const conversationControllers = require("../controllers/conversations");
+
+router.post("/converstions", conversationControllers.createConversation);
+
+router.get("/conversations", conversationControllers.getConversations);
+
+router.get("/conversations", conversationControllers.getConversationDetail);
+
+router.delete("/converstions", conversationControllers.deleteConversation);
+
+module.exports = router;
