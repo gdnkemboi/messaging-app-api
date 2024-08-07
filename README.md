473e78efc1cc336f92c2e406593ba997b705df74
Gideon Kemboi <gedionkemboi18@gmail.com>
2024-08-07 11:21:58 +0300

    Implement production best practices and add README

---
+++ README.md
@@ -0,0 +1,22 @@
+# Chat Application API Built with Express
+
+## Technology Stack
+
+- Node.js
+- Express
+- MongoDB
+
+## Features
+
+- **Signup and Signin**: Using a username or email and password (email is optional).
+- **One-on-One Messaging**: Communicate with other users.
+- **Group Messaging**: Create and participate in group chats.
+- **Adding Contacts**: Easily add and manage contacts.
+- **Notifications**: Receive notifications on important actions.
+
+## TODO
+
+- Implement user search.
+- Implement real-time functionalities (sending messages, messages read status, user online status).
+
+Feel free to contribute or raise issues!
--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const fs = require("fs");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const passport = require("./config/passport");
 const { swaggerUi, swaggerSpec } = require("./config/swagger");
 const cors = require("cors");
+const compression = require("compression");
+const helmet = require("helmet");
 
 main().catch((err) => console.log(err));
 
@@ -24,14 +27,63 @@ const notificationsRouter = require("./routes/notifications");
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(compression());
+
+// Add helmet to the middleware chain with custom CSP directives
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'"],
+    },
+  })
+);
+
+// Create a write stream (in append mode) for logging
+const logDirectory = path.join(__dirname, "log");
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+const accessLogStream = fs.createWriteStream(
+  path.join(logDirectory, "access.log"),
+  { flags: "a" }
+);
+
+// Setup morgan logger
+if (process.env.NODE_ENV === "production") {
+  app.use(logger("combined", { stream: accessLogStream }));
+} else {
+  app.use(logger("dev"));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Configure CORS
-app.use(cors());
+// Define allowed origins for production
+const allowedOrigins = [""];
+
+// CORS options
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps, curl requests)
+    if (!origin) return callback(null, true);
+
+    // Allow requests from allowed origins
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+// Apply CORS middleware
+if (process.env.NODE_ENV === "production") {
+  app.use(cors(corsOptions));
+} else {
+  app.use(cors()); // Allow all origins in development
+}
 
 app.use(passport.initialize());
 
